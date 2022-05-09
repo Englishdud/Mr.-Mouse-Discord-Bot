b486e03c036238a34a2bf35110545f35d55b692a
Englishdud <91146628+Englishdud@users.noreply.github.com>
2022-05-09 18:44:50 -0400

    Couldn't figure out databases...

--- Source Code/main.py
+++ Source Code/main.py
@@ -20,19 +20,6 @@ def get_quote():
   quote = json_data[0]['q'] +  " -" + json_data[0]['a']
   return(quote)
 
-def update_encouragements(encouraging_message):
-  if "encouragements" in db.keys():
-    encouragements = db["encouragements"]
-    encouragements.append(encouraging_message)
-    db["encouragements"] = encouragements
-  else:
-    db["encouragements"] = [encouraging_message]
-
-def delete_encouragement(index):
-  encouragements = db["encouragements"]
-  if len(encouragements) > index:
-    del encouragements[index]
-    db["encouragements"] = encouragements
     
 @client.event
 async def on_ready():
@@ -51,24 +38,9 @@ async def on_message(message):
   if msg.startswith('sudo inspire'):
     await message.channel.send('I ran out of quotes -Rick Astley')
 
-  options = starter_encouragements
-  if "encouragements" in db.keys():
-    options = options + db["encouragements"]
+  
 
   if any(word in msg for word in sad_Words):
-    await message.channel.send(random.choice(options))
+    await message.channel.send(random.choice(starter_encouragements))
 
-  if msg.startswith("sudo new"):
-    encouraging_message = msg.split("sudo new ",1)[1]
-    update_encouragements(encouraging_message)
-    await message.channel.send("Thanks for your submission, you saved lives.")
-
-  if msg.startwith("sudo del"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      index = int(msg.split("sudo del",1)[1])
-      delete_encouragement(index)
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
-
-client.run('OTczMDI5Mzc0OTcyNTQ3MDgy.GIENvT.VVYfwsKZKvWrLC3eAOzEecM5cS8-xp-CQEnq_4')
+client.run('')
